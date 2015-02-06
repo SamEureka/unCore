24053fa5c0e0616057f9458e0fc3a390cc1359e7
Sam <sam@localhost>
2015-02-06 10:50:39 -0500

    yeah... there are lint issues. I'll clean them up later. This is commit #1

---
+++ js/charts/chartFour.js
@@ -0,0 +1,48 @@
+$(function () {
+    $('#chartFour').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2014'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+//                    crop: false,
+                    overflow: 'none'
+                    /*style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'white',
+                        font: '5px Lucida Grande, Lucida Sans Unicode, Verdana, Arial, Helvetica, sans-serif',
+                    }*/
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    });
+});
